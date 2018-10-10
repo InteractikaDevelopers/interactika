9133a95316c8569b5aa66953a23f5726075301aa
Unknown <carlossas_97@hotmail.com>
2018-10-10 09:18:03 -0500

    Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgtUniversalModule } from '@ng-toolkit/universal';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// MODULOS
+import { PagesModule } from './pages/pages.module';
+import { PanelModule } from './panel/panel.module';
+import { FormsModule } from '@angular/forms';
+//RUTAS
+import { APP_ROUTES } from './app.routes';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsModule,
+    PagesModule,
+    PanelModule
   ],
   imports:[
- CommonModule,
-NgtUniversalModule,
- 
-    
+    CommonModule,
+    APP_ROUTES,
+    NgtUniversalModule,
   ],
+  schemas: [NO_ERRORS_SCHEMA],
   providers: [],
 })
 export class AppModule { }
